fe15eb1a584b4b4afff7bca1181b53456df90f97
Thomas Kent <tom@teeks99.com>
2018-11-14 16:07:47 -0600

    RC2

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ TYPE = "beta-rc"
 #TYPE = "rc"
 REPO = "bintray"
 BETA = 1
-RC = 1
+RC = 2
 
 BUILD_DRIVE = "D:" + os.sep
 BUILD_DIR = "ReleaseBuild"
@@ -58,7 +58,7 @@ REPOS = {
             "archive_suffix": "-snapshot"
         },
         "beta-rc": {
-            "url": "https://dl.bintray.com/boostorg/beta/1.{version}.{minor_version}.beta.{beta}.rc{rc}/source/",
+            "url": "https://dl.bintray.com/boostorg/beta/1.{version}.{minor_version}.beta{beta}.rc{rc}/source/",
             "file": "boost_1_{version}_{minor_version}{archive_suffix}.tar.bz2",
             "source_archive_output": "boost_1_{version}_{minor_version}",
             "archive_suffix": "_b{beta}_rc{rc}"
