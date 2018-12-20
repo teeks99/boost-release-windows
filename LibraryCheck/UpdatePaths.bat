96e66c2119d84fe58d32050ff744c7191c5f23d8
Thomas Kent <tom@teeks99.com>
2018-12-19 20:59:44 -0600

    Removed check for VC8/9

--- LibraryCheck/UpdatePaths.bat
+++ LibraryCheck/UpdatePaths.bat
@@ -1,4 +1,4 @@
-::echo off 
+::echo off
 
 REM --- Enter Paths Here ---
 REM The paths must use the windows format '\' backslash, and they must be escaped (a.k.a use
@@ -8,14 +8,6 @@ set BOOST_LIB32_PATH=%BOOST_PATH%\\lib32
 set BOOST_LIB64_PATH=%BOOST_PATH%\\lib64
 
 REM You shouldn't need to edit below here
-bin-sed\sed.exe -i "s/FILL_INC_PATH/%BOOST_PATH%/g" BoostLibraryCheck-VC8.vcproj
-bin-sed\sed.exe -i "s/FILL_32_LINK_PATH/%BOOST_LIB32_PATH%-msvc-8.0/g" BoostLibraryCheck-VC8.vcproj
-bin-sed\sed.exe -i "s/FILL_64_LINK_PATH/%BOOST_LIB64_PATH%-msvc-8.0/g" BoostLibraryCheck-VC8.vcproj
-
-bin-sed\sed.exe -i "s/FILL_INC_PATH/%BOOST_PATH%/g" BoostLibraryCheck-VC9.vcproj
-bin-sed\sed.exe -i "s/FILL_32_LINK_PATH/%BOOST_LIB32_PATH%-msvc-9.0/g" BoostLibraryCheck-VC9.vcproj
-bin-sed\sed.exe -i "s/FILL_64_LINK_PATH/%BOOST_LIB64_PATH%-msvc-9.0/g" BoostLibraryCheck-VC9.vcproj
-
 bin-sed\sed.exe -i "s/FILL_INC_PATH/%BOOST_PATH%/g" BoostLibraryCheck-VC10.vcxproj
 bin-sed\sed.exe -i "s/FILL_32_LINK_PATH/%BOOST_LIB32_PATH%-msvc-10.0/g" BoostLibraryCheck-VC10.vcxproj
 bin-sed\sed.exe -i "s/FILL_64_LINK_PATH/%BOOST_LIB64_PATH%-msvc-10.0/g" BoostLibraryCheck-VC10.vcxproj
