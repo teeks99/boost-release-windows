41ec8b062b925136c78bfe7790ea166a0398d7b3
Thomas Kent <tom@teeks99.com>
2019-07-09 06:48:38 -0500

    Including 14.2 build

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ BUILD_DRIVE = "D:" + os.sep
 BUILD_DIR = "ReleaseBuild"
 TIMES = "times.txt"
 
-vc_versions = ["10.0", "11.0", "12.0", "14.0", "14.1"]
+vc_versions = ["10.0", "11.0", "12.0", "14.0", "14.1", "14.2"]
 vc_archs = ["32", "64"]
 
 PACKAGE_PROCESSES = 16
