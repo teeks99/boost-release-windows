c8cdbccd61d45c2eaa342afe176c906240b90d4b
Thomas Kent <tom@teeks99.com>
2021-03-09 07:45:44 -0600

    Test for 1.76

--- run.py
+++ run.py
@@ -18,11 +18,11 @@ except ImportError: # Python 2
 
 import repo_paths
 
-VERSION = "75"
+VERSION = "76"
 MINOR_VERSION = "0"
-#TYPE = "master-snapshot"
+TYPE = "master-snapshot"
 #TYPE = "beta-rc"
-TYPE = "rc"
+#TYPE = "rc"
 REPO = "bintray"
 BETA = 1
 RC = 1
