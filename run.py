acfb973047550bae00727e19ab130a3d848a8b67
Thomas Kent <tom@teeks99.com>
2021-03-13 06:13:42 -0600

    Work for 1.76

--- run.py
+++ run.py
@@ -20,12 +20,12 @@ import repo_paths
 
 VERSION = "76"
 MINOR_VERSION = "0"
-TYPE = "master-snapshot"
-#TYPE = "beta-rc"
+#TYPE = "master-snapshot"
+TYPE = "beta-rc"
 #TYPE = "rc"
 REPO = "bintray"
 BETA = 1
-RC = 1
+RC = 2
 
 release_extension = "tar.bz2"
 
