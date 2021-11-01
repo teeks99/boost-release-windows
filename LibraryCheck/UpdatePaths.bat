1c2188dae0156dcc326573c3bd40ca51893be248
Thomas Kent <tom@teeks99.com>
2021-11-01 08:49:16 -0500

    14.3 Library Check

--- LibraryCheck/UpdatePaths.bat
+++ LibraryCheck/UpdatePaths.bat
@@ -32,5 +32,9 @@ bin-sed\sed.exe -i "s/FILL_INC_PATH/%BOOST_PATH%/g" BoostLibraryCheck-VC14.2.vcx
 bin-sed\sed.exe -i "s/FILL_32_LINK_PATH/%BOOST_LIB32_PATH%-msvc-14.2/g" BoostLibraryCheck-VC14.2.vcxproj
 bin-sed\sed.exe -i "s/FILL_64_LINK_PATH/%BOOST_LIB64_PATH%-msvc-14.2/g" BoostLibraryCheck-VC14.2.vcxproj
 
+bin-sed\sed.exe -i "s/FILL_INC_PATH/%BOOST_PATH%/g" BoostLibraryCheck-VC14.3.vcxproj
+bin-sed\sed.exe -i "s/FILL_32_LINK_PATH/%BOOST_LIB32_PATH%-msvc-14.3/g" BoostLibraryCheck-VC14.3.vcxproj
+bin-sed\sed.exe -i "s/FILL_64_LINK_PATH/%BOOST_LIB64_PATH%-msvc-14.3/g" BoostLibraryCheck-VC14.3.vcxproj
+
 REM Cleanup garbage leftover by sed
 del sed*
