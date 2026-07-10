cc19e35f9e1212abfa03b7dea49837dc2b21c307
Tom Kent <tom@teeks99.com>
2026-07-10 08:24:21 -0500

    1.92.0 b1 rc1

--- run.py
+++ run.py
@@ -19,12 +19,12 @@ except ImportError: # Python 2
 import repo_paths
 from archive import Archive, run_remote_archive
 
-VERSION = "91"
+VERSION = "92"
 MINOR_VERSION = "0"
 #TYPE = "master-snapshot"
-#TYPE = "beta-rc"
+TYPE = "beta-rc"
 #TYPE = "beta" # Never used, build Beta RC
-TYPE = "rc"
+#TYPE = "rc"
 #TYPE = "release" # Never used, build RC
 REPO = "archives"
 BETA = 1
